2e5ec89cc1810ad673fbd8a43dea8cdeb93fffb0
Sam Shock <sam.shock924@gmail.com>
2023-03-02 15:46:44 -0500

    Router udpates

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,29 @@
 import './App.css';
-// import Header from './Components/Header';
+import {
+	createBrowserRouter,
+	createRoutesFromElements,
+	Route,
+	RouterProvider,
+} from 'react-router-dom';
+
+//pages and components
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
 import Search from './Components/Search';
 import Random from './Components/Random';
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+
+const router = createBrowserRouter(
+	createRoutesFromElements(
+		<Route path='/' element={<Navbar />}>
+			<Route index element={<Home />} />
+			<Route path='search' element={<Search />} />
+			<Route path='random' element={<Random />} />
+		</Route>
+	)
+);
 
 function App() {
-	return (
-		<BrowserRouter>
-			<header>
-				{/* <Header /> */}
-				<Navbar />
-			</header>
-			<main>
-				<Routes>
-					<Route path='/' element={<Home />}></Route>
-					<Route path='search' element={<Search />}></Route>
-					<Route path='random' element={<Random />}></Route>
-				</Routes>
-			</main>
-		</BrowserRouter>
-	);
+	return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,12 +1,17 @@
-import {NavLink} from 'react-router-dom';
+import {NavLink, Outlet} from 'react-router-dom';
 import Header from './Header';
 
 const Navbar = () => {
 	return (
-		<div className='navbar-container'>
-			<Header />
-			<NavLink to='/search'>Search</NavLink>
-			<NavLink to='/random'>Random</NavLink>
+		<div className='root-layout'>
+			<div className='navbar-container'>
+				<Header />
+				<NavLink to='/search'>Search</NavLink>
+				<NavLink to='/random'>Random</NavLink>
+			</div>
+			<main>
+				<Outlet />
+			</main>
 		</div>
 	);
 };
